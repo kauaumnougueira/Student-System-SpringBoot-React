46cfe527a827c5239eb7f46c5753278bf2fb5709
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-05 22:48:39 -0300

    feat: Student class created with theyr attributes and StudentRepository implements

--- src/main/java/kauaNog/Studentsystem/model/repository/StudentRepository.java
+++ src/main/java/kauaNog/Studentsystem/model/repository/StudentRepository.java
@@ -1,5 +1,11 @@
 package kauaNog.Studentsystem.model.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kauaNog.Studentsystem.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
     
 }
