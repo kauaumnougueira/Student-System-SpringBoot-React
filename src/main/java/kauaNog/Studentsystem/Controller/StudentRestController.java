7d5af607e04d558b18641cb23df7b26057b45622
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-10 11:07:11 -0300

    feat: StudentRestController implemented com addStatus();

--- src/main/java/kauaNog/Studentsystem/Controller/StudentRestController.java
+++ src/main/java/kauaNog/Studentsystem/Controller/StudentRestController.java
@@ -1,5 +1,25 @@
 package kauaNog.Studentsystem.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kauaNog.Studentsystem.service.StundentService;
+import kauaNog.Studentsystem.model.Student;
+
+
+
+@RestController
+@RequestMapping("/student")
 public class StudentRestController {
     
+    @Autowired
+    private StundentService studentService;
+
+    @RequestMapping("/add")
+    public String addStatus(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student added";
+    }
 }
