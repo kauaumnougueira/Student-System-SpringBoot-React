7b314c52b60d310e927f64b1b5513223379f83ce
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-10 11:34:24 -0300

    getAllStudents added at RestController

--- src/main/java/kauaNog/Studentsystem/Controller/StudentRestController.java
+++ src/main/java/kauaNog/Studentsystem/Controller/StudentRestController.java
@@ -1,6 +1,9 @@
 package kauaNog.Studentsystem.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,10 @@ public class StudentRestController {
     public String addStatus(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
