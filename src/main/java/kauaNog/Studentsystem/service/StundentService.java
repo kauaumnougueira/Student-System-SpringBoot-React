6602ee7e7d1a0d1aa121046e9bc88c8fef888b22
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-10 10:48:03 -0300

    feat: StudentService and StudentServiceImpl implemented

--- src/main/java/kauaNog/Studentsystem/service/StundentService.java
+++ src/main/java/kauaNog/Studentsystem/service/StundentService.java
@@ -1,5 +1,8 @@
 package kauaNog.Studentsystem.service;
 
+import kauaNog.Studentsystem.model.Student;
+
 public interface StundentService  {
     
+    public Student saveStudent(Student student);
 }
--- src/main/java/kauaNog/Studentsystem/service/impl/StudentServiceImpl.java
+++ src/main/java/kauaNog/Studentsystem/service/impl/StudentServiceImpl.java
@@ -1,5 +1,22 @@
 package kauaNog.Studentsystem.service.impl;
 
-public class StudentServiceImpl {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kauaNog.Studentsystem.model.Student;
+import kauaNog.Studentsystem.model.repository.StudentRepository;
+import kauaNog.Studentsystem.service.StundentService;
+
+@Service
+public class StudentServiceImpl implements StundentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
     
 }
